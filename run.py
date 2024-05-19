b260bf83a7ce24d4382e017a43d262387173734f
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 19:23:16 -0300

    cambios varios

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 import json
-from flask import Flask,request ,render_template, \
-    url_for, jsonify
+from flask import Flask,request , jsonify
 from celery import Celery
 import requests as R
 from os import environ
-from session import Session
 from article import Articles
-from celery.result  import AsyncResult
 
 
 app = Flask(__name__)
 
 redis= environ.get("REDIS","localhost")
+api= environ.get("API","http://localhost:4000")
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://'+redis+':6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://'+redis+':6379/0'
@@ -23,84 +21,45 @@ celery.conf.update(app.config)
 
 
 @celery.task(bind=True)
-def deleteConfirm_as(self,referer,uuid, token):
-    headers = {"X-Access-Tokens":token}
-    r=R.get(referer+"/confirmEmailDelete/"+uuid,headers=headers)
+def deleteConfirm_as(self,uuid):
+    r=R.get(api+"/confirmEmailDelete/"+uuid)
 
 @celery.task(bind=True)
-def taskFinishedArticle(self,referer,uuid, token):
-    headers = {"X-Access-Tokens":token}
-    r=R.put(referer+"/articleFinish/"+uuid,headers=headers)
+def taskFinishedArticle(self,uuid):
+    r=R.put(api+"/articleFinish/"+uuid)
     
 
 @celery.task(bind=True)
-def taskStartedArticle(self,referer,uuid, token):
-    headers = {"X-Access-Tokens":token}
-    r=R.put(referer+"/articleStart/"+uuid,headers=headers)
+def taskStartedArticle(self,uuid):
+    r=R.put(api+"/articleStart/"+uuid)
     
     
 
-
-def url(referer):
-    r=None
-    try:
-
-        link="https://"+referer
-        r=R.get(link+"/ping")
-        return link,202
-    except:
-        try:
-            link="http://"+referer
-            r=R.get(link+"/ping")
-            return link,202
-        except:
-            return None,404
 
 
 @app.route('/deleteConfirm/<string:uuid>')
 def deleteConfirm(uuid):
-    api= request.headers.get("Referer")
-    token = Session().getToken(api)
-    link= Session().getLink(api)
-    deleteConfirm_as.apply_async(kwargs={'referer':link,"uuid":uuid,"token":token},countdown=24*60*60) 
+    deleteConfirm_as.apply_async(kwargs={"uuid":uuid},countdown=24*60*60) 
     return jsonify({"sms":"hola"}),202 
 
-@app.route("/ping")  
-def ping():
-    return jsonify({}),202
-
-@app.route("/login")  
-def login():
-    api= request.headers.get("Referer")
-    link,r= url(api)
-    if(r==404):
-        return jsonify({"error":"La url esta mal o el servidor desconectado"}),404 
-    Session().addHost(api,request.headers.get("X-Access-Tokens"),link)
-    return jsonify({}),202
 
 @app.route("/finishedArticle",methods=["POST"])
 def finishedArticle():
-    api= request.headers.get("Referer")
-    token = Session().getToken(api)
-    link = Session().getLink(api)
     data= request.get_json()
     id = Articles().getTaskId(data.get("article"))
     if  id != None:
         celery.control.revoke(id,terminate=True,signal="SIGKILL")
-    task=taskFinishedArticle.apply_async(kwargs={'referer':link,"uuid":data.get("article"),"token":token},countdown=data.get("time")) 
+    task=taskFinishedArticle.apply_async(kwargs={"uuid":data.get("article")},countdown=data.get("time")) 
     Articles().addArticle(data.get("article"),str(task))
     return jsonify({"sms":"hola"}),202 
 
 @app.route("/startedArticle",methods=["POST"])
 def startedArticle():
-    api= request.headers.get("Referer")
-    token = Session().getToken(api)
-    link = Session().getLink(api)
     data= request.get_json()
     id = Articles().getTaskId(data.get("article"))
     if  id != None:
         celery.control.revoke(id,terminate=True,signal="SIGKILL")
-    task=taskStartedArticle.apply_async(kwargs={'referer':link,"uuid":data.get("article"),"token":token},countdown=data.get("time")) 
+    task=taskStartedArticle.apply_async(kwargs={"uuid":data.get("article")},countdown=data.get("time")) 
     Articles().addArticle(data.get("article"),str(task))
     return jsonify({"sms":"hola"}),202 
 
