95116f22acaccf76616bca0e0aa9beb3926ba2b7
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 21:09:22 -0300

    logs

--- run.py
+++ run.py
@@ -86,5 +86,5 @@ def startedArticle():
 if __name__ == '__main__':
     from waitress import serve
     # app.run(host="0.0.0.0", port=5000, debug=True)
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5000, debug=True)
  
