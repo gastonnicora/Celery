0ab936b99560ad47b45678951dd72f62947787bf
gastonnicora <gastonmatias.21@gmail.com>
2024-06-30 02:50:35 -0300

    prueba celery

--- run.py
+++ run.py
@@ -28,44 +28,60 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 # Definición de tareas de Celery
+
 @celery.task(bind=True)
 def deleteConfirm_as(self, uuid):
     headers = {'content-type': 'application/json'}
-    r= R.get(api_url + "/confirmEmailDelete/" + uuid)
-    sleep(1)
     try:
+        r = R.get(api_url + "/confirmEmailDelete/" + uuid)
+        r.raise_for_status()  
         return r.status_code, r.json()
-    except:
-        return r.status_code, r.text
-
+    except Exception as e:
+        print(f"Error al hacer la solicitud DELETE Confirm: {str(e)}")
+        return 500, str(e)  
 @celery.task(bind=True)
 def taskFinishedArticle(self, data):
     headers = {'x-access-tokens': data["token"]}
-    r= R.put(api_url + "/articleFinish/" + data["article"], headers=headers)
     try:
+        r = R.put(api_url + "/articleFinish/" + data["article"], headers=headers)
+        r.raise_for_status()
         return r.status_code, r.json()
-    except:
-        return r.status_code, r.text
+    except Exception as e:
+        print(f"Error al hacer la solicitud PUT Finished Article: {str(e)}")
+        return 500, str(e)
 
 @celery.task(bind=True)
 def taskStartedArticle(self, data):
     headers = {'x-access-tokens': data["token"]}
-    r= R.put(api_url + "/articleStart/" + data["article"], headers=headers)
     try:
+        r = R.put(api_url + "/articleStart/" + data["article"], headers=headers)
+        r.raise_for_status()
         return r.status_code, r.json()
-    except:
-        return r.status_code, r.text
+    except Exception as e:
+        print(f"Error al hacer la solicitud PUT Started Article: {str(e)}")
+        return 500, str(e)
 
 @celery.task(bind=True)
 def taskStartedAuction(self, data):
-    sleep(1)
     headers = {'x-access-tokens': data["token"]}
-    print(api_url)
-    r= R.put(api_url + "/auctionStart/" + data["article"], headers=headers, timeout=30)
     try:
+        r = R.put(api_url + "/auctionStart/" + data["article"], headers=headers, timeout=30)
+        r.raise_for_status()
         return r.status_code, r.json()
-    except:
-        return r.status_code, r.text
+    except Exception as e:
+        print(f"Error al hacer la solicitud PUT Started Auction: {str(e)}")
+        return 500, str(e)
+
+@celery.task(bind=True)
+def taskFinishedAuction(self, data):
+    headers = {'x-access-tokens': data["token"]}
+    try:
+        r = R.put(api_url + "/auctionFinished/" + data["article"], headers=headers)
+        r.raise_for_status()
+        return r.status_code, r.json()
+    except Exception as e:
+        print(f"Error al hacer la solicitud PUT Finished Auction: {str(e)}")
+        return 500, str(e)
 
 @celery.task(bind=True)
 def taskFinishedAuction(self, data):
