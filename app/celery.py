3e13b97a0613029c89afeef6aaa8d8023430226e
gastonnicora <gastonmatias.21@gmail.com>
2024-11-04 21:58:20 -0300

    reintento

--- app/celery.py
+++ app/celery.py
@@ -39,8 +39,13 @@ def taskFinishedArticle(self, data):
         r.raise_for_status()
         return r.status_code, r.json()
     except Exception as e:
-        print(f"Error al hacer la solicitud PUT Finished Article: {str(e)}")
-        return 500, str(e)
+        try:
+            r = R.put(api_url + "/articleFinish/" + data["article"], headers=headers)
+            r.raise_for_status()
+            return r.status_code, r.json()
+        except Exception as e:
+            print(f"Error al hacer la solicitud PUT Finished Article: {str(e)}")
+            return 500, str(e)
 
 @celery.task(bind=True)
 def taskStartedArticle(self, data):
@@ -50,8 +55,13 @@ def taskStartedArticle(self, data):
         r.raise_for_status()
         return r.status_code, r.json()
     except Exception as e:
-        print(f"Error al hacer la solicitud PUT Started Article: {str(e)}")
-        return 500, str(e)
+        try:
+            r = R.put(api_url + "/articleStart/" + data["article"], headers=headers)
+            r.raise_for_status()
+            return r.status_code, r.json()
+        except Exception as e:
+            print(f"Error al hacer la solicitud PUT Started Article: {str(e)}")
+            return 500, str(e)
 
 @celery.task(bind=True)
 def taskStartedAuction(self, data):
@@ -61,8 +71,13 @@ def taskStartedAuction(self, data):
         r.raise_for_status()
         return r.status_code, r.json()
     except Exception as e:
-        print(f"Error al hacer la solicitud PUT Started Auction: {str(e)}")
-        return 500, str(e)
+        try:
+            r = R.put(api_url + "/auctionStart/" + data["article"], headers=headers, timeout=30)
+            r.raise_for_status()
+            return r.status_code, r.json()
+        except Exception as e:
+            print(f"Error al hacer la solicitud PUT Started Auction: {str(e)}")
+            return 500, str(e)
 
 @celery.task(bind=True)
 def taskFinishedAuction(self, data):
@@ -72,14 +87,12 @@ def taskFinishedAuction(self, data):
         r.raise_for_status()
         return r.status_code, r.json()
     except Exception as e:
-        print(f"Error al hacer la solicitud PUT Finished Auction: {str(e)}")
-        return 500, str(e)
+        try:
+            r = R.put(api_url + "/auctionFinished/" + data["article"], headers=headers)
+            r.raise_for_status()
+            return r.status_code, r.json()
+        except Exception as e:
+            print(f"Error al hacer la solicitud PUT Finished Auction: {str(e)}")
+            return 500, str(e)
 
-@celery.task(bind=True)
-def taskFinishedAuction(self, data):
-    headers = {'x-access-tokens': data["token"]}
-    r= R.put(api_url + "/auctionFinished/" + data["article"], headers=headers)
-    try:
-        return r.status_code, r.json()
-    except:
-        return r.status_code, r.text
+
